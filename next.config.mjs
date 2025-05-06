bf7daee534c8f1eb6252b5051b76ac50d75b99e7
devadh21 <devadh21@gmail.com>
2025-05-06 20:09:23 +0000

    update netlify toml

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
-const { withNetlify } = require('@netlify/next');
 
-module.exports = withNetlify({
-  reactStrictMode: true,
-  // other Next.js config options
-});
 
 
 const nextConfig = {
